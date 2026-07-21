29403e836468c97d02e3bf0a7f716f3939ccd35c
syandbb <syandbb@syandbbs-MacBook-Air.local>
2026-07-21 14:24:41 -0400

    trapping

---
+++ two_pointer/11.trapping_rain_water.ts
@@ -0,0 +1,6 @@
+function trap(height: number[]): number {
+    /*
+    the trick to this problem is that we start off at opposite ends. Arbitrarily move the left if the left and right equal. otherwise move the end that is lesser of the two. 
+    */
+
+};
